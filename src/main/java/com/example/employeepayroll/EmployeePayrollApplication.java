f988d040c1a481d036135b3a2be16508b65dab38
user name <user email>
2021-06-27 21:47:59 +0530

    Logging using Lombok By slf4j Properties

--- src/main/java/com/example/employeepayroll/EmployeePayrollApplication.java
+++ src/main/java/com/example/employeepayroll/EmployeePayrollApplication.java
@@ -2,12 +2,18 @@ package com.example.employeepayroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @SpringBootApplication
 public class EmployeePayrollApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EmployeePayrollApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(EmployeePayrollApplication.class, args);
+		log.info("Employee Payroll app started in {} environment",
+				  context.getEnvironment().getProperty("environment"));
 		System.out.println("employee payroll deployed successfully");
 	}
 
