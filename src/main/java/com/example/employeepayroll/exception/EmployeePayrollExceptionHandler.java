96b61e3bb44dc29a2b395133585ef13ab199dd45
user name <user email>
2021-06-29 12:34:01 +0530

    Ability to Handle Date Format Exception

--- src/main/java/com/example/employeepayroll/exception/EmployeePayrollExceptionHandler.java
+++ src/main/java/com/example/employeepayroll/exception/EmployeePayrollExceptionHandler.java
@@ -5,6 +5,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -14,6 +15,15 @@ import com.example.employeepayroll.DTO.ResponseDTO;
 
 @ControllerAdvice
 public class EmployeePayrollExceptionHandler {
+	private static final String message = "Exception while processing rest request";
+
+	@ExceptionHandler(HttpMessageNotReadableException.class)
+	public ResponseEntity<ResponseDTO> handleHttpMessageNotReadableException(HttpMessageNotReadableException exception){
+		ResponseDTO responseDTO = new ResponseDTO(message,"Date should have been in the right format");
+		return new ResponseEntity<ResponseDTO>(responseDTO,HttpStatus.BAD_REQUEST);
+		
+	}
+	
 	@ExceptionHandler(MethodArgumentNotValidException.class)
 	public ResponseEntity<ResponseDTO> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception){
 		List<ObjectError> errorList = exception.getBindingResult().getAllErrors();
